e27195d9f9e9b1022e39246d6eab80fdee9b3a8e
emireyes <141955808+emireyes@users.noreply.github.com>
2023-09-14 12:32:33 -0600

    Add files via upload

---
+++ do_do-while_for.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main ()
+{
+	int letreros =0,num=0;
+	printf("Numero de letreros:");
+	scanf("%d",&letreros);
+	while(num<letreros)
+	{
+		printf("Hola mundo\n");
+		num++;
+	}
+	
+	return 0;
+}
+
+#include <stdio.h>
+
+intmain ()
+{
+	int letreros=0, num=0;
+	printf("numero de letreros:");
+	scanf("%d",&letreros);
+	do{
+		printf("hola mundo\n");
+		num++;
+	}while(num<letreros);
+	return 0;
+}
+
+#include <stdio.h>
+
+int main ()
+{
+	int letreros=0, num=0;
+	printf("Numero de letreros:");
+	scanf("%d",&letreros);
+	for (int x=1<=letreros;x++)
+		printf("hola mundo\n");
+	return 0;
+}
